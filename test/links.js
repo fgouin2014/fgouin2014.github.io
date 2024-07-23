7dc6a6d8a74641cf20eaab82bb5313e855c5b5f5
fgouin2014 <fgouin2014@gmail.com>
2024-07-23 13:12:04 -0400

    modification du bouton menu
    Modification de Menu

---
+++ test/links.js
@@ -0,0 +1,6 @@
+// Liste de liens prédéfinie
+const links = [
+    { name: "Google", url: "https://www.google.com" },
+    { name: "GitHub", url: "https://www.github.com" },
+    { name: "Stack Overflow", url: "https://stackoverflow.com" }
+];
