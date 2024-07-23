25273bc8fa2ee85e60a82e873470b97614b11dc8
fgouin2014 <fgouin2014@gmail.com>
2024-07-23 13:22:24 -0400

    Revert "modification du bouton menu"
    This reverts commit 7dc6a6d8a74641cf20eaab82bb5313e855c5b5f5.

--- test/links.js
+++
@@ -1,6 +0,0 @@
-// Liste de liens prédéfinie
-const links = [
-    { name: "Google", url: "https://www.google.com" },
-    { name: "GitHub", url: "https://www.github.com" },
-    { name: "Stack Overflow", url: "https://stackoverflow.com" }
-];
